3e9b576e49a85063acbe6a5fecf6823997a591e0
Lawrence Liu <lwrncliu@gmail.com>
2021-04-16 20:52:51 -0600

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,13 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 15 14:03:23 2021
-
-@author: lwrnc
-load data functions
-"""
-import torch
 import json
-from transformers import LayoutLMTokenizerFast
+import os
+import pandas as pd
 
 def read_ocr_tagged_file(filepath):
     """
@@ -46,6 +39,35 @@ def read_label_file(filepath):
         label_output = json.loads(text)
     return label_output
 
+def get_train_data():
+    #change the path here if you are not FRED
+    path_1 = "/scratch/fs1493/mlu_project/SROIE/0325updated.task1train(626p)/"
+    path_2 = "/scratch/fs1493/mlu_project/SROIE/0325updated.task2train(626p)/"
+    
+    #list of all files .txt & .jpg from task1 (images + bounding boxes)
+    files = [name for name in os.listdir(path_1) if os.path.isfile(path_1 + name)]
+    #txt files with bounding box information
+    ocr_tagged_filenames = set([name[:-4] for name in files if name[-3:] == "txt" and name[-5] != ")"])
+    #list of all files .txt & .jpg from task2 (images + ground truth)
+    files_2 = [name for name in os.listdir(path_2) if os.path.isfile(path_2 + name)]
+    #all images files from task2
+    image_filenames = set([name[:-4] for name in files if name[-3:] == "jpg" and name[-5] != ")"])
+    #all ground truth files from task 2
+    label_filenames = set([name[:-4] for name in files if name[-3:] == "txt" and name[-5] != ")"])
+            
+    #files names of all txt ids that appear in task1 & task2
+    names = ocr_tagged_filenames.intersection(label_filenames)
+    
+    labels = []
+    df2 = pd.DataFrame(inex = names, columns = {'ocr_output'})
+    for name in names:
+        df2.at[name, 'ocr_output'] = read_ocr_tagged_file(path_1 + name + ".txt") 
+        labels += [read_label_file(path_2 + name + ".txt")]
+        df1 = pd.DataFrame(labels,index=names)
+    
+    train_df = pd.concat([df1, df2], axis=1).rename_axis('file_name').reset_index()
+    return train_df
+
 
 def process_for_encode(dataset):
     """
@@ -67,7 +89,7 @@ def process_for_encode(dataset):
         address = dataset[keys[2]][i]
         total = dataset[keys[3]][i]
         value_str = " ".join([company, date, address, total])
-        i_input = keys[0] + " : " + keys[1] + " : " + keys[2] + " : " + keys[3] + " : " + value_str
+        i_input = keys[0] + " : " + keys[1] + " : " + keys[2] + " : " + keys[3] + " : " + value_str #see example for why this format: https://github.com/NielsRogge/Transformers-Tutorials/blob/master/LayoutLM/Fine_tuning_LayoutLMForTokenClassification_on_FUNSD.ipynb
         processed_question.append(i_input)
     return processed_passage, processed_question
 
@@ -87,15 +109,6 @@ def encode_data(dataset, tokenizer, max_seq_length=64):
     batch_encoding = tokenizer(questions, passages, padding = "max_length", truncation = True, max_length = max_seq_length, return_tensors = "pt")
     
     return batch_encoding
-
-
-# def extract_labels(dataset):
-#     """Converts labels into numerical labels.
-
-#   Args:
-#     dataset: A Pandas dataframe containing the labels in the column 'label'.
-
-#   Returns:
 #     labels: A list of integers corresponding to the labels for each example,
 #       where 0 is False and 1 is True.
 #   """
