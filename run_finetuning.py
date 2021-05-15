364cecc72814cbeaf44eecaa93c6daec703680c1
Lawrence Liu <lwrncliu@gmail.com>
2021-05-15 00:25:47 -0500

    2 loops

--- run_finetuning.py
+++ run_finetuning.py
@@ -11,35 +11,42 @@ df = load_data.get_data()
 
 tokenizer = LayoutLMTokenizerFast.from_pretrained("microsoft/layoutlm-base-uncased")
 
-aug_params = {
-    "copies" : 3,
-    "p_lines" : 1.0, #100%
-    "p_char" : 1.0, #100%
-}
 
-sroie_dataset = sroie.SROIE_Dataset(df, tokenizer,augmentation = aug_params)
 
-train_size = int(0.7*len(sroie_dataset))
-test_size = int(0.2*len(sroie_dataset))
-valid_size = len(sroie_dataset) - train_size - test_size
-train_data, test_data, valid_data = torch.utils.data.random_split(sroie_dataset, [train_size, test_size, valid_size])
+#augmentation type loop
+for k in [[0,0,0], [2,0.8,0.8], [3,0.8,0.8]]:
+    print("augmentation parameters: ", k)
+    aug_params = {
+    "copies" : k[0],
+    "p_lines" : k[1], #100%
+    "p_char" : k[2], #100%
+    }
+    
+    sroie_dataset = sroie.SROIE_Dataset(df, tokenizer, augmentation = aug_params)
 
-training_args = TrainingArguments(
-    output_dir = '/scratch/kl2487/mllu',
-    num_train_epochs = 5,
-    per_device_train_batch_size = 16,
-    evaluation_strategy = "epoch",
-    do_predict=True
-    )
-trainer = Trainer(
-    model_init = finetuning_utils.model_init,
-    train_dataset = train_data,
-    eval_dataset = valid_data,
-    tokenizer = tokenizer,
-    compute_metrics = finetuning_utils.compute_metrics,
-    args = training_args
-    )
-trainer.train()
-predictions=trainer.predict(valid_data)
-predictions.metrics
-print(predictions)
+    #sample size loop
+    for n in [25, 50, 75, 100, 200, 300, 400]:
+        train_size = n
+        test_size = 0.15*len(sroie_dataset)
+        train_data, test_data = torch.utils.data.random_split(sroie_dataset, [train_size, test_size])
+
+        training_args = TrainingArguments(
+            output_dir = '/scratch/kl2487/mllu',
+            num_train_epochs = 5,
+            per_device_train_batch_size = 16,
+            evaluation_strategy = "epoch",
+            do_predict=True
+            )
+        trainer = Trainer(
+            model_init = finetuning_utils.model_init,
+            train_dataset = train_data,
+            eval_dataset = test_data,
+            tokenizer = tokenizer,
+            compute_metrics = finetuning_utils.compute_metrics,
+            args = training_args
+            )
+        print("fine-tuning with ", n, "samples")
+        trainer.train()
+        #predictions=trainer.predict(valid_data)
+        #predictions.metrics
+        #print(predictions)
